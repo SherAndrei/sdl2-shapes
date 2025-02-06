773dd1c08c95b385c99c8b120a522c31b27d0847
SherAndrei <welldryus@gmail.com>
2025-02-06 13:13:16 +0300

    initial commit

---
+++ errm.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#define ERR_1(S)                                                               \
+  fprintf(stderr, "[%s:%d] %s error: %s\n", __FILE__, __LINE__, #S,            \
+          SDL_GetError());
+
+#define ERR_2(S, SEVTY)                                                        \
+  do {                                                                         \
+    ERR_1(S)                                                                   \
+    if (SEVTY)                                                                 \
+      abort();                                                                 \
+  } while (0)
+
+#define GET_3RD_ARG(A1, A2, A3, ...) A3
+#define ERR_MACRO_CHOOSER(...) GET_3RD_ARG(__VA_ARGS__, ERR_2, ERR_1)
+#define ERR(...)                                                               \
+  do {                                                                         \
+    ERR_MACRO_CHOOSER(__VA_ARGS__)(__VA_ARGS__);                               \
+  } while (0)
